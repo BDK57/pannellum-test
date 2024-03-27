b97c8fa8448f29fe2cddc8f675f9b037e91d9be1
Hassan <badshahdev57@gmail.com>
2024-03-27 14:52:56 +0500

    Fix Build Problem

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import { useRouter } from 'next/navigation'; // Import useRouter from 'next/router' instead of 'next/navigation'
 
 const Page = () => {
@@ -8,12 +8,49 @@ const Page = () => {
   const [uploadStatus, setUploadStatus] = useState('');
   const [folderName, setFolderName] = useState('');
   const router = useRouter();
+  const inputFileRef = useRef(null); // Create a ref for the input element
+
 
   const handleFileChange = (event) => {
     setFiles(event.target.files);
   };
 
-  const handleUpload = async () => {
+//   const handleUpload = async () => {
+//     if (!files) return;
+
+//     const formData = new FormData();
+//     for (const file of files) {
+//       formData.append('files[]', file);
+//     }
+
+//     setUploading(true);
+//     setUploadStatus('Accepting images...');
+
+//     try {
+//       const response = await fetch('http://127.0.0.1:5000/upload', {
+//         method: 'POST',
+//         body: formData
+//       });
+
+//       const data = await response.json();
+//       setUploading(false);
+
+//       if (response.ok) {
+//         setFiles(null); // Clear the input field
+//         setFolderName(data.folder_name);
+//         setUploadStatus(`Folder: ${data.folder}, Execution Time: ${data.execution_time} seconds`);
+
+//         router.push(`/Para?folderName=${data.folder_name}`);
+//       } else {
+//         setUploadStatus(`Error: ${data.error}`);
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//       setUploading(false);
+//       setUploadStatus('An error occurred while uploading');
+//     }
+//   };
+const handleUpload = async () => {
     if (!files) return;
 
     const formData = new FormData();
@@ -35,10 +72,9 @@ const Page = () => {
 
       if (response.ok) {
         setFiles(null); // Clear the input field
+        inputFileRef.current.value = '';
         setFolderName(data.folder_name);
         setUploadStatus(`Folder: ${data.folder}, Execution Time: ${data.execution_time} seconds`);
-
-        router.push(`/Para?folderName=${data.folder_name}`);
       } else {
         setUploadStatus(`Error: ${data.error}`);
       }
@@ -51,11 +87,11 @@ const Page = () => {
 
   return (
     <div>
-      <input type="file" onChange={handleFileChange} multiple />
+      <input type="file" onChange={handleFileChange} multiple ref={inputFileRef} />
       <button onClick={handleUpload} disabled={uploading}>Upload</button>
       <div>{uploading && 'Uploading...'}</div>
       <div>{uploadStatus}</div>
-      <div>Folder Name: {folderName}</div>
+      <div>{folderName && `Folder Name: ${folderName}`}</div>
     </div>
   );
 };
